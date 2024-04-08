ecb0422e5cd1367cfec0d1eab3f76dd73b6e3f94
Mateusz Nejman <mateusz_nejman@hotmail.com>
2024-04-08 18:16:34 +0200

    Random dithering

--- docs/js/piskel-packaged-2024-04-08-06-12.js
+++ docs/js/piskel-packaged-2024-04-08-06-12.js
@@ -33585,46 +33585,51 @@ ns.ToolsHelper = {
  * @require pskl.utils
  */
 (function() {
-    var ns = $.namespace('pskl.tools.drawing');
-  
-    ns.DitheringToolNew = function() {
-      ns.SimplePen.call(this);
-      this.toolId = 'tool-dithering-new';
-      this.helpText = 'Color Dithering tool';
-      this.shortcut = pskl.service.keyboard.Shortcuts.TOOL.DITHERING_NEW;
-    };
-  
-    pskl.utils.inherit(ns.DitheringToolNew, ns.SimplePen);
-  
-    ns.DitheringToolNew.prototype.supportsDynamicPenSize = function() {
-      return true;
-    };
-  
-    /**
-     * @override
-     */
-    ns.DitheringToolNew.prototype.applyToolAt = function(col, row, frame, overlay, event) {
-      this.previousCol = col;
-      this.previousRow = row;
-  
-      var penSize = pskl.app.penSizeService.getPenSize();
-      var points = pskl.PixelUtils.resizePixel(col, row, penSize);
-      points.forEach(function (point) {
-        this.applyToolOnPixel(point[0], point[1], frame, overlay, event);
-      }.bind(this));
-    };
-  
-    ns.DitheringToolNew.prototype.applyToolOnPixel = function(col, row, frame, overlay, event) {
-      var ditheringColor = window.tinycolor(pskl.app.selectedColorsService.getPrimaryColor());
-      ditheringColor.setAlpha(Math.random());
-      console.log(pskl.app.selectedColorsService.getPrimaryColor());
-      console.log(ditheringColor);
+  var ns = $.namespace('pskl.tools.drawing');
 
-      this.draw(ditheringColor.toRgbString(), col, row, frame, overlay);
-    };
-  
-  })();
-  ;(function () {
+  ns.DitheringToolNew = function() {
+    ns.SimplePen.call(this);
+    this.toolId = 'tool-dithering-new';
+    this.helpText = 'Random Dithering tool';
+    this.shortcut = pskl.service.keyboard.Shortcuts.TOOL.DITHERING_NEW;
+  };
+
+  pskl.utils.inherit(ns.DitheringToolNew, ns.SimplePen);
+
+  ns.DitheringToolNew.prototype.supportsDynamicPenSize = function() {
+    return true;
+  };
+
+  /**
+   * @override
+   */
+  ns.DitheringToolNew.prototype.applyToolAt = function(col, row, frame, overlay, event) {
+    this.previousCol = col;
+    this.previousRow = row;
+
+    var penSize = pskl.app.penSizeService.getPenSize();
+    var points = pskl.PixelUtils.resizePixel(col, row, penSize);
+    points.forEach(function (point) {
+      this.applyToolOnPixel(point[0], point[1], frame, overlay, event);
+    }.bind(this));
+  };
+
+  ns.DitheringToolNew.prototype.applyToolOnPixel = function(col, row, frame, overlay, event) {
+    var usePrimaryColor = Math.random() < 0.5;
+
+    if (pskl.app.mouseStateService.isRightButtonPressed()) {
+      usePrimaryColor = !usePrimaryColor;
+    }
+
+    var ditheringColor = usePrimaryColor ?
+      pskl.app.selectedColorsService.getPrimaryColor() :
+      pskl.app.selectedColorsService.getSecondaryColor();
+
+    this.draw(ditheringColor, col, row, frame, overlay);
+  };
+
+})();
+;(function () {
   var ns = $.namespace('pskl.tools.transform');
 
   ns.AbstractTransformTool = function () {};
