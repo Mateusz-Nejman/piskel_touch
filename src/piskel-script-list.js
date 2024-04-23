c13dfe118423556004373dcbeb211af237e105ea
Mateusz Nejman <mateusz_nejman@hotmail.com>
2024-04-23 08:56:23 +0200

    Added touch-punch library for jquery; Fixed FrameList sortable

--- src/piskel-script-list.js
+++ src/piskel-script-list.js
@@ -4,6 +4,7 @@
   // Core libraries
   "js/lib/jquery-1.8.0.js",
   "js/lib/jquery-ui-1.10.3.custom.js",
+  "js/lib/jquery.ui.touch-punch.js",
   "js/lib/pubsub.js",
   "js/lib/bootstrap/bootstrap.js",
 
